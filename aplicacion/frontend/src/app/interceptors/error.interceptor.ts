dc2d9be6c3396d145f34fa7296db4f0a31f5090d
seqqueman@yahoo.es <miguel-angel.sequedo@getronics.com>
2021-02-09 10:30:39 +0100

    bug:correccion fallo fichero interceptor

--- aplicacion/frontend/src/app/interceptors/error.interceptor.ts
+++ aplicacion/frontend/src/app/interceptors/error.interceptor.ts
@@ -13,7 +13,6 @@ export class ErrorInterceptor implements HttpInterceptor {
             catchError((error: HttpErrorResponse) => {
                 if((error.status === 0 && error.url && error.url.includes('/back')) || error.status === 401 || error.status === 403) {
 
-                    const urlpage = this.router.routerState.snapshot;
                     // window.location.href = 'https://pase-pru.carm.es/pase/login?service=http%3A%2F%2Flocalhost%3A4200%3Asegunda';
                     window.location.href = 'https://pase-pru.carm.es/pase/login?service=http%3A%2F%2Flocalhost%3A8080%2Fdemo-angular%2Flogin%2Fcas';//%3Asegunda';
                     // https://pase-pru.carm.es/pase/login?service=https%3A%2F%2Fdelfos-pru.carm.es%2Fdelfos%2Flogin%2Fcas                  
