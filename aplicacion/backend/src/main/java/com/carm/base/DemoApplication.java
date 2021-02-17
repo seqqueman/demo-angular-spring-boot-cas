aa41b34a0954fab957a390305b09366215f80d92
seqqueman@yahoo.es <miguel-angel.sequedo@getronics.com>
2021-02-17 13:29:34 +0100

    feat:Habilitado del cacheo de staticos
    También se habilita las url estilo html5, sin
    usar el hash #

--- aplicacion/backend/src/main/java/com/carm/base/DemoApplication.java
+++ aplicacion/backend/src/main/java/com/carm/base/DemoApplication.java
@@ -1,5 +1,8 @@
 package com.carm.base;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,36 +12,41 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
 
 @SpringBootApplication
 public class DemoApplication extends SpringBootServletInitializer {
-	
+
 	@Autowired
 	BuildProperties buildProperties;
-	
+
 //	@Value("${aplicacion.servidor}")
 //	private String servidor;
-	
-	
+
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(DemoApplication.class);
 	}
-    
-    // De esta manera seteamos el context path, util en desarrollo, para controlar 
-    // mejor los errores de cros origin, al levantar desde el IDE o desplegar directamente
-    // el war en tomcat
+
+	// De esta manera seteamos el context path, util en desarrollo, para controlar
+	// mejor los errores de cros origin, al levantar desde el IDE o desplegar
+	// directamente
+	// el war en tomcat
 	@Bean
-    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory>
-        webServerFactoryCustomizer() {
-        return factory -> factory.setContextPath("/"+buildProperties.get("applicationName"));
-    }
-	
+	public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() {
+		return factory -> factory.setContextPath("/" + buildProperties.get("applicationName"));
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	
-	
+
 //	@Bean
 //    public WebMvcConfigurer CORSConfigurer(){
 //        return new WebMvcConfigurer() {
@@ -51,5 +59,30 @@ public class DemoApplication extends SpringBootServletInitializer {
 //        };
 //    }
 
-	
+	@Bean
+	public WebMvcConfigurer ResourcesConfigurer() {
+		return new WebMvcConfigurer() {
+			/*
+			 * De esta manera delegamos en el routing de angular las rutas que no pertenecen
+			 * a Spring, al mismo tiempo podemos activar el cacheo de los estaticos de
+			 * angular
+			 */
+			@Override
+			public void addResourceHandlers(ResourceHandlerRegistry registry) {
+				registry.addResourceHandler("/**")
+						.addResourceLocations("classpath:/public/")
+						.setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS))
+						.resourceChain(true)
+						.addResolver(new PathResourceResolver() {
+							@Override
+							protected Resource getResource(String resourcePath, Resource location) throws IOException {
+								Resource requestedResource = location.createRelative(resourcePath);
+								return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+										: new ClassPathResource("/public/index.html");
+							}
+						}).addResolver(new VersionResourceResolver().addContentVersionStrategy("/**"));
+			}
+		};
+	}
+
 }
--- aplicacion/backend/src/main/java/com/carm/base/WebSecurityConfig.java
+++ aplicacion/backend/src/main/java/com/carm/base/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package com.carm.base;
 
 import java.util.Collections;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSessionEvent;
 
 //import org.jasig.cas.client.session.SingleSignOutFilter;
@@ -34,17 +33,16 @@ import com.carm.base.security.AppCasAssertionUserDetailsService;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	BuildProperties buildProperties;
-	
 
 	@Value("${aplicacion.servidor}")
 	private String servidor;
-	
+
 	@Value("${pase.login}")
 	private String paseLogin;
-	
+
 	@Value("${pase.logout}")
 	private String paseLogout;
 
@@ -52,111 +50,73 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private AjaxAwareAuthEntryPointWrapper ajaxAwareAuthEntryPointWrapper;
-	
-//	@Override
-//    public void configure(WebSecurity web) throws Exception {
-//        web.ignoring(). //
-//            antMatchers(HttpMethod.OPTIONS, "/**"). //
-//            antMatchers("/"). //
-//            antMatchers("/*.{js,html}"). //
-//            antMatchers("/img/**"). //
-//            antMatchers("/node_modules/**"). //
-//            antMatchers("/**/*.{js,html,css}");
-//    }
 
-	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		logger.info("dentro security");
-		
-//		http
-//		.csrf().disable()
-//		.httpBasic()
-//		.authenticationEntryPoint(ajaxAwareAuthEntryPointWrapper)
-//		.and()
-//		.authorizeRequests()
-//		.antMatchers("/*", "*/**","/login/**").permitAll()
-//		.regexMatchers("/back.*").hasAnyRole("ROLE_USER","ROLE_ADMIN")
-//		;
-		
+
 
 		http
 //		.cors().and()
-		.authorizeRequests()
-		.regexMatchers("/back.*").hasAnyRole("USER","ADMIN")
+			.authorizeRequests()
+//		 .antMatchers("/","/public/**", "/css/**","/webjars/**").permitAll()
+			.regexMatchers("/back.*").hasAnyRole("USER", "ADMIN")
 //		.authenticated()
-		.anyRequest()
-		.permitAll()
-		.and()
-		.httpBasic()
-		.authenticationEntryPoint(ajaxAwareAuthEntryPointWrapper);
-		
-		http.csrf().disable();
-		
-		
-		http.
-			logout().
-			logoutSuccessUrl("/").invalidateHttpSession(true)
-		.deleteCookies("JSESSIONID");
-		
-//		http.logout()
-////		.logoutUrl(buildProperties.get("applicationName")+"/logout/cas")
-//		.logoutSuccessHandler((httpServletRequest, httpServletResponse, authentication) -> {
-//			httpServletResponse.addHeader("Set-Cookie", "JSESSIONID=; Max-Age=0; Expires=Thu, 01-Jan-1970 00:00:10 GMT");
-//			httpServletResponse.setStatus(HttpServletResponse.SC_OK);
-//		})
-//		.logoutSuccessUrl("/");
-		
-		http.addFilterAt(this.casFilter(), CasAuthenticationFilter.class);
+			.anyRequest().permitAll()
+			.and()
+			.httpBasic()
+			.authenticationEntryPoint(ajaxAwareAuthEntryPointWrapper);
 
+		http.csrf().disable();
+
+		http.logout().logoutSuccessUrl("/").invalidateHttpSession(true).deleteCookies("JSESSIONID");
+
+		http.addFilterAt(this.casFilter(), CasAuthenticationFilter.class);
 		http.addFilterBefore(this.logoutFilter(), LogoutFilter.class);
 		http.addFilterBefore(this.singleSignOutFilter(), CasAuthenticationFilter.class);
 	}
-	
+
 	@Bean
-    public AppCasAssertionUserDetailsService authenticationUserDetailsService() {
-        return new AppCasAssertionUserDetailsService();
-    }
+	public AppCasAssertionUserDetailsService authenticationUserDetailsService() {
+		return new AppCasAssertionUserDetailsService();
+	}
 
-	
 	@Bean
-    public CasAuthenticationFilter casFilter() throws Exception {
-        CasAuthenticationFilter filter = new CasAuthenticationFilter();
-        filter.setAuthenticationManager(this.authenticationManager());
-        
-        return filter;
-    }
+	public CasAuthenticationFilter casFilter() throws Exception {
+		CasAuthenticationFilter filter = new CasAuthenticationFilter();
+		filter.setAuthenticationManager(this.authenticationManager());
 
-    @Bean
-    public ServiceProperties serviceProperties() {
-        logger.info("service properties");
-        ServiceProperties serviceProperties = new ServiceProperties();
-        serviceProperties.setService(servidor+buildProperties.get("applicationName")+"/login/cas");
-        serviceProperties.setSendRenew(false);
-        return serviceProperties;
-    }
+		return filter;
+	}
 
-    @Bean
-    public TicketValidator ticketValidator() {
-        return new Cas30ServiceTicketValidator(paseLogin);
-    }
+	@Bean
+	public ServiceProperties serviceProperties() {
+		logger.info("service properties");
+		ServiceProperties serviceProperties = new ServiceProperties();
+		serviceProperties.setService(servidor + buildProperties.get("applicationName") + "/login/cas");
+		serviceProperties.setSendRenew(false);
+		return serviceProperties;
+	}
 
-    @Bean
-    public CasAuthenticationProvider casAuthenticationProvider() {
-        CasAuthenticationProvider provider = new CasAuthenticationProvider();
-        provider.setServiceProperties(serviceProperties());
-        provider.setTicketValidator(ticketValidator());
-        provider.setAuthenticationUserDetailsService(
-        		this.authenticationUserDetailsService()
+	@Bean
+	public TicketValidator ticketValidator() {
+		return new Cas30ServiceTicketValidator(paseLogin);
+	}
+
+	@Bean
+	public CasAuthenticationProvider casAuthenticationProvider() {
+		CasAuthenticationProvider provider = new CasAuthenticationProvider();
+		provider.setServiceProperties(serviceProperties());
+		provider.setTicketValidator(ticketValidator());
+		provider.setAuthenticationUserDetailsService(this.authenticationUserDetailsService()
 //          s -> new AppUserDetails("80069088Q", AuthorityUtils.createAuthorityList("ROLE_USER"), "msg88q", "", false, false, false, true)
-        );
-        provider.setKey("CAS_PROVIDER_LOCALHOST_8900");
-        return provider;
-    }
+		);
+		provider.setKey("CAS_PROVIDER_LOCALHOST_8900");
+		return provider;
+	}
 
-
-    @Bean
-    public AuthenticationEntryPoint casEntryPoint() {
+	@Bean
+	public AuthenticationEntryPoint casEntryPoint() {
 		CasAuthenticationEntryPoint entryPoint = new CasAuthenticationEntryPoint();
 		entryPoint.setLoginUrl(paseLogin);
 		entryPoint.setServiceProperties(serviceProperties());
@@ -168,29 +128,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected AuthenticationManager authenticationManager() throws Exception {
 		return new ProviderManager(Collections.singletonList(this.casAuthenticationProvider()));
 	}
-	
-    
-    @Bean
-    public SingleSignOutFilter singleSignOutFilter() {
-        return new SingleSignOutFilter();
-    }
-    
-    @Bean
+
+	@Bean
+	public SingleSignOutFilter singleSignOutFilter() {
+		return new SingleSignOutFilter();
+	}
+
+	@Bean
 	public SecurityContextLogoutHandler securityContextLogoutHandler() {
 		return new SecurityContextLogoutHandler();
 	}
-	
+
 	@Bean
 	public LogoutFilter logoutFilter() {
-		String logincas=servidor+buildProperties.get("applicationName")+"/login/cas";
-	    LogoutFilter logoutFilter = new LogoutFilter( paseLogout+"?service="+logincas, this.securityContextLogoutHandler());
-	    logoutFilter.setFilterProcessesUrl("/logout/cas");
-	    return logoutFilter;
+		String logincas = servidor + buildProperties.get("applicationName") + "/login/cas";
+		LogoutFilter logoutFilter = new LogoutFilter(paseLogout + "?service=" + logincas,
+				this.securityContextLogoutHandler());
+		logoutFilter.setFilterProcessesUrl("/logout/cas");
+		return logoutFilter;
 	}
-	
+
 	@EventListener
 	public SingleSignOutHttpSessionListener singleSignOutHttpSessionListener(HttpSessionEvent event) {
-	    return new SingleSignOutHttpSessionListener();
+		return new SingleSignOutHttpSessionListener();
 	}
 
 }
